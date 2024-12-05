a0aeb8f1c0ce3efa9d04a8269b8acbcb9caee153
Shenol07 <shenolshengov41@gmail.com>
2024-12-05 22:33:16 +0200

    Add project structure

---
+++ src/main/java/bg/shengov/garages_manager/GaragesManagerApplication.java
@@ -0,0 +1,13 @@
+package bg.shengov.garages_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragesManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/shengov/garages_manager/GaragesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.shengov.garages_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
