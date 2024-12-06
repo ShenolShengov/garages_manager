f661797fb040a21197cb6e620cb672466f1c3e63
Shenol07 <shenolshengov41@gmail.com>
2024-12-06 20:24:22 +0200

    Add model mapper

--- src/main/java/bg/shengov/garages_manager/service/impl/CarServiceImpl.java
+++ src/main/java/bg/shengov/garages_manager/service/impl/CarServiceImpl.java
@@ -7,41 +7,31 @@ import bg.shengov.garages_manager.model.entity.Car;
 import bg.shengov.garages_manager.repository.CarRepository;
 import bg.shengov.garages_manager.service.CarService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.BeanUtils;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PagedModel;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
 public class CarServiceImpl implements CarService {
 
     private final CarRepository carRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     public Car create(CarCreateDTO carCreateDTO) {
-        Car created = toCar(carCreateDTO);
-        return carRepository.save(created);
+        return carRepository.save(modelMapper.map(carCreateDTO, Car.class));
     }
-
-    private static Car toCar(CarCreateDTO carCreateDTO) {
-        Car created = new Car();
-        BeanUtils.copyProperties(carCreateDTO, created);
-        return created;
-    }
-
-    private static CarDetailInfoDTO toDetailsInfo(Car car) {
-        CarDetailInfoDTO carDetailInfoDTO = new CarDetailInfoDTO();
-        BeanUtils.copyProperties(car, carDetailInfoDTO);
-        return carDetailInfoDTO;
+    private Function<Car, CarDetailInfoDTO> toDetailsInfo() {
+        return e -> modelMapper.map(e, CarDetailInfoDTO.class);
     }
 
     @Override
     public PageableResponse<CarDetailInfoDTO> all(Pageable pageable) {
-        Page<CarDetailInfoDTO> page = carRepository.findAll(pageable).map(CarServiceImpl::toDetailsInfo);
+        Page<CarDetailInfoDTO> page = carRepository.findAll(pageable).map(toDetailsInfo());
         return PageableResponse.from(page);
-//        return Page<CarDetailInfoDTO> all = carRepository.findAll(pageable).map(CarServiceImpl::toDetailsInfo);
-//        return new PagedModel<>(all);
     }
 }
