ff897480183b626ff39ca869956d0561a8f11308
Shenol07 <shenolshengov41@gmail.com>
2024-12-21 22:05:12 +0200

    Update CompoundFilter

--- src/main/java/bg/shengov/garages_manager/service/specificaiton/filter/CompoundFilter.java
+++ src/main/java/bg/shengov/garages_manager/service/specificaiton/filter/CompoundFilter.java
@@ -4,17 +4,30 @@ import org.springframework.data.jpa.domain.Specification;
 
 import java.util.Optional;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class CompoundFilter<K, T> implements FilterCriteria<T> {
 
     private final K first;
     private final K second;
     private final BiFunction<K, K, Specification<T>> specificationBuilder;
+    private Function<K, Specification<T>> firstSpecificationBuilder;
+    private Function<K, Specification<T>> secondSpecificationBuilder;
 
     public CompoundFilter(K first, K second, BiFunction<K, K, Specification<T>> specificationBuilder) {
         this.first = first;
         this.second = second;
         this.specificationBuilder = specificationBuilder;
+    }
+
+    public CompoundFilter(K first, K second, BiFunction<K, K, Specification<T>> specificationBuilder,
+                          Function<K, Specification<T>> firstSpecificationBuilder,
+                          Function<K, Specification<T>> secondSpecificationBuilder) {
+        this.first = first;
+        this.second = second;
+        this.specificationBuilder = specificationBuilder;
+        this.firstSpecificationBuilder = firstSpecificationBuilder;
+        this.secondSpecificationBuilder = secondSpecificationBuilder;
     }
 
     public boolean isValidFilter() {
@@ -23,6 +36,15 @@ public class CompoundFilter<K, T> implements FilterCriteria<T> {
 
     @Override
     public Optional<Specification<T>> toSpecification() {
-        return isValidFilter() ? Optional.of(specificationBuilder.apply(first, second)) : Optional.empty();
+        if (isValidFilter()) {
+            return Optional.of(specificationBuilder.apply(first, second));
+        }
+        if (first != null) {
+            return Optional.of(firstSpecificationBuilder.apply(first));
+        }
+        if (second != null) {
+            return Optional.of(secondSpecificationBuilder.apply(second));
+        }
+        return Optional.empty();
     }
 }
